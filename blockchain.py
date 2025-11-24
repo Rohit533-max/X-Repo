9be5d1a3ab75e6a23d56dfcdffdb41f0f0c99c52
Rohit533-max <rohit03383@gmail.com>
2025-11-24 20:33:18 +0530

    Structure of a single block

--- blockchain.py
+++ blockchain.py
@@ -18,4 +18,18 @@ class Blockchain(object):
     def last_block(self):
         #returns the last block in the chain
         pass
+    #here how a single block looks like
+    block = {
+        'index' : 1,
+        'timestamp' : 1506057125.900786, #one dig change
+        'transactions' : [
+            {
+                'sender' : "8527147fe1f5426f9dd545de4b27ee00",
+                 'receiver' : "a77f5cdfa2934df3954a5c7c7da5df1f",
+
+            }
+        ],
+        'proof' : 324984774000,
+        'previous_hash' :"a77f5cdfa2934df3954a5c7c7da5df1f",
+    }
     
