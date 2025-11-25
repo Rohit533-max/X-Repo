13b73b37d63f0c19ea10dd0c5fd1d4f3e2c24f56
Rohit533-max <rohit03383@gmail.com>
2025-11-25 20:15:49 +0530

    The seed block of chain in created

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,37 @@
 #a class whose constructor creates an initial emply list(to store our blockchain), and antoher to store the transictions
+
+#creating the first block which has no predecessors, the seed block
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
 
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+        #creating the seed block
+    def new_block(self,proof,previous_hash = None):
+        """
+        Create a new block in the blockchain
+        :param proof : <int> the Proof given by proof of Work algorithm
+        :param previous_hash: (optional) <str> hash of previous block
+        :return : <dict> New block
+        """
+        block ={
+            'index' : len(self.chain) +1,
+            'timestamp': time(),
+            'transactions':self.current_transactions,
+            'proof':proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        #reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block 
 
     def new_block(self):
         #create a new Block and adds it to the chain 
@@ -27,11 +55,20 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         #hashes a block
-        pass
+        """
+        Docstring for hash
+        
+        Creates a SHA-256 hash of a Block
+        :param block : <dict>block
+        :return : <str>
+        """
+        #To ensure that the dictionary is ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     @property
     def last_block(self):
         #returns the last block in the chain
-        pass
+        return self.chain[-1]
     #here how a single block looks like
     block = {
         'index' : 1,
@@ -40,9 +77,11 @@ class Blockchain(object):
             {
                 'sender' : "8527147fe1f5426f9dd545de4b27ee00",
                  'receiver' : "a77f5cdfa2934df3954a5c7c7da5df1f",
+                 'amount' : 5,
 
             }
         ],
         'proof' : 324984774000,
         'previous_hash' :"a77f5cdfa2934df3954a5c7c7da5df1f",
+        #each block within itself contain the hash of its previous block
     }
