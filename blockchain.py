9a5605c45824947139bcc3d4b9604ea44fcc36bf
Rohit533-max <rohit03383@gmail.com>
2025-11-26 21:51:26 +0530

    The base touch of the chain

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ import hashlib
 import json
 from textwrap import dedent
 from time import time
+from urllib import response
 from uuid import uuid4
 
 from flask import Flask , jsonify, request
@@ -233,7 +234,37 @@ def full_chain():
     }
     return jsonify(response), 200
 
+@app.route('/nodes/register', methods = ['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+    for node in nodes:
+        blockchain.register_node(node)
+
+    reponse = {
+        'message' : 'New nodes have been added',
+        'total_nodes' : list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message' : 'Our chain is authoritative',
+            'new_chain' : blockchain.chain
+        }
+        return jsonify(response), 200
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port =5000)
 
-app.run(debug=True)
